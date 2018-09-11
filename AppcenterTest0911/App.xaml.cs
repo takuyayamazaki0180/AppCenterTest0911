8699cfaff7a07fcae6c5c960f5663ecc5a50230c
takuyayamazaki0180 <43163529+takuyayamazaki0180@users.noreply.github.com>
2018-09-11 15:16:49 +0900

    Add files via upload

---
+++ AppcenterTest0911/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppcenterTest0911
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("ios=ec50a3f2-90cf-46e3-bd97-51ab8980664c;" + "uwp={Your UWP App secret here};" + "android={Your Android App secret here}", typeof(Analytics), typeof(Crashes), typeof(Distribute));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
